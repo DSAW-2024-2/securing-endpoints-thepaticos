b3b449f005b4697a49c7d2ec981b20b552c70a52
Nicolas <njcaceres04@gmail.com>
2024-09-27 08:54:56 -0500

    functional state

--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -1,7 +1,5 @@
 const jwt = require("jsonwebtoken");
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 
 exports.cookieJwtAuth = (req, res, next) => {
   try {
@@ -10,10 +8,14 @@ exports.cookieJwtAuth = (req, res, next) => {
       if (!token) {
         return res.status(403).json({ message: "You are not authorized" });
       }
-      const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-      req.user = user;
+      const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+          return res.status(403).json({ message: "Access Denied: Invalid Token" });
+        }
+        req.user = user;
+        next();
+      });
     }
-    next();
   } catch (err) {
     res.clearCookie("authToken");
     return res.status(403).json({ message: "You are not authorized" });
