a8eaec2d2dd4548bd4d58e7305c353c7ca2956be
Jhojan Jimenez <jhojanjimene@gmail.com>
2024-10-01 12:09:13 -0500

    Deploy en Vercel

--- middleware/cookieJwtAuth.js
+++ middleware/cookieJwtAuth.js
@@ -3,16 +3,14 @@ require("dotenv").config();
 
 exports.cookieJwtAuth = (req, res, next) => {
   try {
-    if (req.method !== "GET") {
-      const authHeader = req.headers["cookie"];
-      const token = authHeader && authHeader.split(" ")[1];
-      if (!token) {
-        return res.status(401).json({ message: "You are not authorized" });
-      } else {
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-      }
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "You are not authorized" });
+    } else {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      next();
     }
-    next();
   } catch (err) {
     res.clearCookie("authToken");
     return res.status(403).json({ message: "You are not authorized" });
